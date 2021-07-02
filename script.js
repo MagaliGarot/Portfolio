fb69fe81f5c22aa95918fed608d87e0c021fdcc9
Magali Garot <magali.garot@gmail.com>
2021-07-02 22:56:43 +0200

    correction mediaqueries

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// HOVER PARALLAX for FORMATIONS
+// HOVER PARALLAX 
 //Documentation : codepen : https://codepen.io/oscicen/pen/zyJeJw
 (function() {
     document.addEventListener("mousemove", parallax);
@@ -30,7 +30,61 @@
         document.getElementById("cardWorkSix").style.cssText = "height: 720px; margin-top: 0px"; 
         document.getElementById("cardWorkSeven").style.cssText = "height: 720px; margin-top: 0px"; 
         document.getElementById("cardWorkHeight").style.cssText = "height: 720px; margin-top: 0px";
-        // document.getElementById("accessibilityTrue").style.display = "none";
-        // document.getElementById("accessibilityFalse").style.display = "block";
     });
 })();
+
+// var fireflies = 25;
+// var $container = $(".container");
+// var $containerWidth = $container.width();
+// var $containerHeight = $container.height();
+// var master = new TimelineMax();
+
+// for (var i = 0; i < fireflies; i++) {
+//   var firefly = $('<div class="firefly"></div>');
+//   TweenLite.set(firefly, {
+//     x: Math.random() * $containerWidth,
+//     y: Math.random() * $containerHeight
+//   });
+//   $container.append(firefly);
+//   flyTheFirefly(firefly);
+// }
+
+// function flyTheFirefly(elm) {
+//   var flyTl = new TimelineMax();
+//   var fadeTl = new TimelineMax({
+//     delay: Math.floor(Math.random() * 2) + 1,
+//     repeatDelay: Math.floor(Math.random() * 6) + 1,
+//     repeat: -1
+//   });
+
+//   fadeTl.to(
+//     [elm],
+//     0.25,
+//     { opacity: 0.25, yoyo: true, repeat: 1, repeatDelay: 0.2, yoyo: true },
+//     Math.floor(Math.random() * 6) + 1
+//   );
+  
+//   flyTl
+//     .set(elm, {scale: Math.random() * 0.75 + 0.5})
+//     .to(elm, Math.random() * 100 + 100, {
+//     bezier: {
+//       values: [
+//         {
+//           x: Math.random() * $containerWidth,
+//           y: Math.random() * $containerHeight
+//         },
+//         {
+//           x: Math.random() * $containerWidth,
+//           y: Math.random() * $containerHeight
+//         }
+//       ]
+//     },
+//     onComplete: flyTheFirefly,
+//     onCompleteParams: [elm]
+//   });
+// }
+
+
+//https://codepen.io/Mertl/pen/GexapP
+//https://codepen.io/slyka85/pen/BJEbVL
+
